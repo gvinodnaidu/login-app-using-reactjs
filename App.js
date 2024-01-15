b522e7a3a73cb530da0d1e6cacb684044285f265
gvinodnaidu <66511259+gvinodnaidu@users.noreply.github.com>
2024-01-15 23:27:59 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "Header";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="header">
+      <Header />;
+    </div>
+  );
+}
+
+export default App;
